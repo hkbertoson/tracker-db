9c66e2136e8905e48753c5c84e930f4aeafe4221
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-09-01 11:12:15 -0500

    Updated Types

--- pages/api/requests/update/[id].ts
+++ pages/api/requests/update/[id].ts
@@ -5,11 +5,9 @@ export default async function handle(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const requestID = req.query.id;
+	const requestID = req.query.id as any;
 	const request = await prisma.requests.update({
 		where: {id: requestID},
-		data: {
-            
-        },
+		data: {},
 	});
 }
