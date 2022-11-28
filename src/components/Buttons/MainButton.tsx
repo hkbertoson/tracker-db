8c4ffefd703870001806f51c459a0ba52078a786
Hunter Bertoson <Hunterkylebertoson@gmail.com>
2022-11-28 13:23:03 -0600

    Updated request page

--- src/components/Buttons/MainButton.tsx
+++ src/components/Buttons/MainButton.tsx
@@ -3,18 +3,18 @@ export default function MainButton() {
 	return (
 		<div className="flex flex-row gap-2 items-center mt-1">
 			<Link href="/">
-				<button className="bg-blue-300 hover:bg-blue-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+				<button className="bg-blue-300 hover:bg-blue-400 text-gray-800 font-bold py-2 px-4 rounded-full">
 					Home
 				</button>
 			</Link>
 			<Link href="/requests">
-				<button className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+				<button className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-full">
 					Back
 				</button>
 			</Link>
 			<button
 				type="submit"
-				className="bg-orange-300 hover:bg-orange-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+				className="bg-orange-300 hover:bg-orange-400 text-gray-800 font-bold py-2 px-4 rounded-full">
 				Submit
 			</button>
 		</div>
--- src/pages/requests/view/[id].tsx
+++ src/pages/requests/view/[id].tsx
@@ -2,37 +2,54 @@ import prisma from '@/utils/prisma';
 import {RequestPageProps} from '@/utils/types';
 import Link from 'next/link';
 import {Header} from '@/components/Header';
+import MainButton from '@/components/Buttons/MainButton';
 
 export default function RequestPage(data: RequestPageProps) {
 	return (
 		<>
-			<Header title={data.project_id} url="/" />
-			<div className="grid grid-cols-12">
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
-				<label htmlFor="project_id">Project ID</label>
-				<input className="form-input" id="project_id" />
+			<h1 className="text-3xl text-center">
+				<Link href="/">View Request: {data.project_id}</Link>
+			</h1>
+			<div className="text-center flex flex-col w-1/2 m-auto gap-1">
+				<label>Name: {data.name}</label>
+				<label>Project ID: {data.project_id}</label>
+				<label>Account Name: {data.account_name}</label>
+				<label>WBS: {data.wbs || ''}</label>
+				<label>Total Hours: {data.total_hours_spent}</label>
+				<label>Name: {data.name}</label>
+				<label>Comments: {data.comment}</label>
+				<label>Created at: {data.created_at.toString()}</label>
+				<label>Last Updated: {data.last_updated.toString()}</label>
 			</div>
-			<div className="hidden sm:block" aria-hidden="true">
-				<div className="py-5">
-					<div className="border-t border-gray-200" />
+
+			<div className="flex gap-1 justify-center">
+				<div className="flex flex-row gap-2 items-center mt-1">
+					<Link href="/">
+						<button className="bg-blue-300 hover:bg-blue-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+							Home
+						</button>
+					</Link>
+					<Link href="/requests">
+						<button className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+							Back
+						</button>
+					</Link>
+					<button
+						type="submit"
+						className="bg-orange-300 hover:bg-orange-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+						Submit
+					</button>
+					<Link href={`/requests/update/${data.id}`}>
+						<button className="bg-red-300 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+							Update
+						</button>
+					</Link>
 				</div>
 			</div>
 		</>
 	);
 }
+
 export async function getServerSideProps({params}: any) {
 	const data = await prisma.requests.findUnique({
 		where: {id: parseInt(params.id)},
